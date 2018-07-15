ee6954a0496264f10fdc610dab891e778a6e38f7
Pavel <silinp@yandex.ru>
2018-07-15 14:09:56 +0300

    PID controller && manual tunning

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,24 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID(): p_error(0.), i_error(0.), d_error(0.), Kp(0.), Ki(0.), Kd(0.)
+{}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  return - p_error * Kp - i_error * Ki - d_error * Kd;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.1, 0.001, 6.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -50,7 +51,10 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
+
+          pid.UpdateError(cte);
+
+          double steer_value = pid.TotalError();
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
