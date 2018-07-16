e7692b41c052cdb615082f591ff05a691959f2af
Pavel <silinp@yandex.ru>
2018-07-16 06:29:16 +0300

    delete video files

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,7 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
   pid.Init(0.1, 0.001, 6.0);
+  //pid.Init(0.1, 0.000, 8.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
